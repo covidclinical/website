491ed069e4789adffbf220b4d1015d2f2ab488ab
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-04-07 17:16:15 -0400

    Update logos

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -6,8 +6,10 @@ bundle exec jekyll build
 
 if [ "$1" == "--prod" ]; then
     echo "* For production: syncing with bucket '$S3_BUCKET_4CE_PROD'"
+    rm -f _site/robots.txt
     aws s3 sync _site/ s3://$S3_BUCKET_4CE_PROD/
 else
     echo "* For development: syncing with bucket '$S3_BUCKET_4CE_DEV'"
+    echo -e "User-agent: *\nDisallow: /" > _site/robots.txt
     aws s3 sync _site/ s3://$S3_BUCKET_4CE_DEV/ --cache-control max-age=0
 fi
