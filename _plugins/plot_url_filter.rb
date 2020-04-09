0d444d8b4708f810dcb719c4055bdefd66da6a27
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-04-09 14:02:28 -0400

    Add support for lineup plots, move external js/css files to the repository

--- _plugins/plot_url_filter.rb
+++ _plugins/plot_url_filter.rb
@@ -1,15 +1,22 @@
 
-module Jekyll::PlotURLFilter
+module Jekyll::C19Filters
     include Jekyll::Filters::URLFilters
 
-    def plot_url(input, page_url)
-      relative_page_dir = File.dirname(relative_url(page_url))
-      "#{relative_page_dir}/vega/#{input}.json"
+    def to_plot_id(plot_path)
+      plot_id = plot_path.sub("/", "_").sub(".", "_")
+      plot_id
     end
 
-    def to_release_dir(input)
-      File.basename(File.dirname(relative_url(input)))
+    def to_plot_url(input, page_url)
+      relative_page_dir = File.dirname(relative_url(page_url))
+      "#{relative_page_dir}/#{input}"
     end
+
+    def to_plot_type(input)
+      plot_type = File.dirname(input)
+      plot_type
+    end
+
 end
 
-Liquid::Template.register_filter(Jekyll::PlotURLFilter)
+Liquid::Template.register_filter(Jekyll::C19Filters)
